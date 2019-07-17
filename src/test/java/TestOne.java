2df90f300c5f9d1df7dbd6711a6d219a70b6ab32
Jags <jags14385@users.noreply.github.com>
2019-07-17 23:02:46 +1000

    More formatting

--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -1,10 +1,10 @@
+import static java.lang.Thread.sleep;
+
 import junit.framework.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import static java.lang.Thread.sleep;
 
 public class TestOne extends TestCase {
   private WebDriver driver;
@@ -19,7 +19,7 @@ public class TestOne extends TestCase {
     WebElement searchBox = driver.findElement(By.name("q"));
     searchBox.sendKeys("SecurePay");
     searchBox.submit();
-    sleep(2000);  // Let the user actually see something!
+    sleep(2000); // Let the user actually see something!
     driver.quit();
   }
 }
